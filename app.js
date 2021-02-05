0f8664c108fa871cbb840b47aef678828f871956
Manjunatha Patkar <manjunathapatkar150@gmail.com>
2021-02-05 16:49:20 +0530

    Adding npm and request module to project

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
-console.log("Starting...")
-setTimeout(()=>{
-console.log("Inside function")
-},2000)
-setTimeout(()=>{
-console.log("inside sencond timeout")
-},0)
+// console.log("Starting...")
+// setTimeout(()=>{
+// console.log("Inside function")
+// },2000)
+// setTimeout(()=>{
+// console.log("inside sencond timeout")
+// },0)
 
-console.log("Stopping...")
+// console.log("Stopping...")
+//weather app code
+const request=require('request')
