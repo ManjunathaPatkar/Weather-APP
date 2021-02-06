4770e6dfd953e5ccf540f48a097b1b4f5f2a5f51
Manjunatha Patkar <manjunathapatkar150@gmail.com>
2021-02-06 19:56:20 +0530

    using Accuweather to get location key and use it to extract weather info

--- app.js
+++ app.js
@@ -9,16 +9,21 @@
 // console.log("Stopping...")
 //weather app code
 const request=require('request')
-const url ='https://data.climacell.co/v4/locations?apikey=VJkiT8gJ2c68MCwm3BSG6MkDOlu1YBBv'
-request({url:url},(error,response)=>{
+const url ='http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=SDhu1AbAnfwcdEeo9m1y67jA3p1AqXXX&q=34.0544%2C-118.2439'
+request({url:url,json:true},(error,response)=>{
     // const data = JSON.parse(response.body)
     // console.log(data)
-    console.log(response.currently)
+    const locationkey=response.body.Key
+    const url1 ='http://dataservice.accuweather.com/forecasts/v1/daily/1day/'+locationkey+'?apikey=SDhu1AbAnfwcdEeo9m1y67jA3p1AqXXX'
+    request({url:url1,json:true},(error,response)=>{
+        console.log("Minimum temperature "+response.body.DailyForecasts[0].Temperature.Minimum.Value)
+        console.log("Maximum temperature " + response.body.DailyForecasts[0].Temperature.Maximum.Value)
+    })
 })
 const url1 ='https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoibWFuanVuYXRoYXBhdGthciIsImEiOiJja2t0NzU5cDUwa2lrMnFwNjlldmR0Ymc4In0.T3OhyQ0q1bG_IWYsd2mpxg'
 request({ url: url1, json: true }, (err,response)=>{
     // console.log(response)
     const res=response.body
-    console.log(res.features[0].center[0])
-    console.log(res.features[0].center[1])
+    console.log("latitude "+res.features[0].center[0])
+    console.log("longitude "+res.features[0].center[1])
 })
