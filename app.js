8345adfe88502ab9a54838da7fa47682c9e2e068
Manjunatha Patkar <manjunathapatkar150@gmail.com>
2021-02-07 16:17:18 +0530

    Adding debug statements to handle in case of error like network or api failure

--- app.js
+++ app.js
@@ -13,17 +13,31 @@ const url ='http://dataservice.accuweather.com/locations/v1/cities/geoposition/s
 request({url:url,json:true},(error,response)=>{
     // const data = JSON.parse(response.body)
     // console.log(data)
-    const locationkey=response.body.Key
-    const url1 ='http://dataservice.accuweather.com/forecasts/v1/daily/1day/'+locationkey+'?apikey=SDhu1AbAnfwcdEeo9m1y67jA3p1AqXXX'
-    request({url:url1,json:true},(error,response)=>{
-        console.log("Minimum temperature "+response.body.DailyForecasts[0].Temperature.Minimum.Value)
-        console.log("Maximum temperature " + response.body.DailyForecasts[0].Temperature.Maximum.Value)
-    })
+    // console.log(error)
+    if(error){
+        console.log("Unable to connect to weather data")
+    }
+    else {
+        const locationkey = response.body.Key
+        const url1 = 'http://dataservice.accuweather.com/forecasts/v1/daily/1day/' + locationkey + '?apikey=SDhu1AbAnfwcdEeo9m1y67jA3p1AqXXX'
+        request({ url: url1, json: true }, (error, response) => {
+            console.log("Minimum temperature " + response.body.DailyForecasts[0].Temperature.Minimum.Value)
+            console.log("Maximum temperature " + response.body.DailyForecasts[0].Temperature.Maximum.Value)
+        })
+    }
+    /*
+    */
 })
 const url1 ='https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoibWFuanVuYXRoYXBhdGthciIsImEiOiJja2t0NzU5cDUwa2lrMnFwNjlldmR0Ymc4In0.T3OhyQ0q1bG_IWYsd2mpxg'
 request({ url: url1, json: true }, (err,response)=>{
     // console.log(response)
-    const res=response.body
-    console.log("latitude "+res.features[0].center[0])
-    console.log("longitude "+res.features[0].center[1])
+    if (err){
+        console.log("unable to connect to the mapbox api")
+    }
+    else {
+        const res = response.body
+        console.log("latitude " + res.features[0].center[0])
+        console.log("longitude " + res.features[0].center[1])
+    }
+    
 })
