1a76fd7e5f988b7fa0b00d1d44d2aaf91dccb18f
Manjunatha Patkar <manjunathapatkar150@gmail.com>
2021-02-07 16:44:19 +0530

    Error handling in mapbox when given place is invalid

--- app.js
+++ app.js
@@ -10,6 +10,7 @@
 //weather app code
 const request=require('request')
 const url ='http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=SDhu1AbAnfwcdEeo9m1y67jA3p1AqXXX&q=34.0544%2C-118.2439'
+//const url = 'http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=SDhu1AbAnfwcdEeo9m1y67jA3p1AqXXX&q=34.0544'
 request({url:url,json:true},(error,response)=>{
     // const data = JSON.parse(response.body)
     // console.log(data)
@@ -17,13 +18,18 @@ request({url:url,json:true},(error,response)=>{
     if(error){
         console.log("Unable to connect to weather data")
     }
+    else if(response.body.Message){
+        console.log(response.body.Message)
+    }
     else {
-        const locationkey = response.body.Key
-        const url1 = 'http://dataservice.accuweather.com/forecasts/v1/daily/1day/' + locationkey + '?apikey=SDhu1AbAnfwcdEeo9m1y67jA3p1AqXXX'
-        request({ url: url1, json: true }, (error, response) => {
-            console.log("Minimum temperature " + response.body.DailyForecasts[0].Temperature.Minimum.Value)
-            console.log("Maximum temperature " + response.body.DailyForecasts[0].Temperature.Maximum.Value)
-        })
+        
+            const locationkey = response.body.Key
+            const url1 = 'http://dataservice.accuweather.com/forecasts/v1/daily/1day/' + locationkey + '?apikey=SDhu1AbAnfwcdEeo9m1y67jA3p1AqXXX'
+            request({ url: url1, json: true }, (error, response) => {
+                console.log("Minimum temperature " + response.body.DailyForecasts[0].Temperature.Minimum.Value)
+                console.log("Maximum temperature " + response.body.DailyForecasts[0].Temperature.Maximum.Value)
+            })
+        
     }
     /*
     */
@@ -33,6 +39,9 @@ request({ url: url1, json: true }, (err,response)=>{
     // console.log(response)
     if (err){
         console.log("unable to connect to the mapbox api")
+    }
+    else if(response.body.message){
+        console.log("please check the entered place name")
     }
     else {
         const res = response.body
