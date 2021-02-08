3643bd28c9cc5a6bf09462d7b9abd67a2f7047e5
Manjunatha Patkar <manjunathapatkar150@gmail.com>
2021-02-08 11:57:25 +0530

    handling when argument not provided

--- app.js
+++ app.js
@@ -3,14 +3,20 @@ const geocode=require('./utils/geocode')
 const forecast=require('./utils/forecast')
 
 const address=process.argv[2]
-geocode(address,(err,data)=>{
-    if (err){
-        return console.log(err)
-    }
-    forecast(data.latitude, data.longitude, (error, forecastdata) => {
-        if(error){
-            return console.log(error)
+
+if(address===undefined){
+    console.log("Please provide an address")
+}
+else{
+    geocode(address, (err, data) => {
+        if (err) {
+            return console.log(err)
         }
-        console.log("The minimum and maximum temperature of "+data.place+" is "+forecastdata.minTemp+" and "+forecastdata.maxTemp)
+        forecast(data.latitude, data.longitude, (error, forecastdata) => {
+            if (error) {
+                return console.log(error)
+            }
+            console.log("The minimum and maximum temperature of " + data.place + " is " + forecastdata.minTemp + " and " + forecastdata.maxTemp)
+        })
     })
-})
+}
