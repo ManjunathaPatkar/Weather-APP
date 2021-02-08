621779c42c65c83356a39fa5ec7c8c33c45c7154
Manjunatha Patkar <manjunathapatkar150@gmail.com>
2021-02-08 10:24:54 +0530

    changed code east to read and added functions for mapbox request

--- app.js
+++ app.js
@@ -1,52 +1,70 @@
-// console.log("Starting...")
-// setTimeout(()=>{
-// console.log("Inside function")
-// },2000)
-// setTimeout(()=>{
-// console.log("inside sencond timeout")
-// },0)
-
-// console.log("Stopping...")
 //weather app code
 const request=require('request')
-const url ='http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=SDhu1AbAnfwcdEeo9m1y67jA3p1AqXXX&q=34.0544%2C-118.2439'
-//const url = 'http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=SDhu1AbAnfwcdEeo9m1y67jA3p1AqXXX&q=34.0544'
-request({url:url,json:true},(error,response)=>{
-    // const data = JSON.parse(response.body)
-    // console.log(data)
-    // console.log(error)
-    if(error){
-        console.log("Unable to connect to weather data")
-    }
-    else if(response.body.Message){
-        console.log(response.body.Message)
-    }
-    else {
+const geocode=require('./utils/geocode')
+// const url ='http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=SDhu1AbAnfwcdEeo9m1y67jA3p1AqXXX&q=34.0544%2C-118.2439'
+// //const url = 'http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=SDhu1AbAnfwcdEeo9m1y67jA3p1AqXXX&q=34.0544'
+// request({url:url,json:true},(error,response)=>{
+//     // const data = JSON.parse(response.body)
+//     // console.log(data)
+//     // console.log(error)
+//     if(error){
+//         console.log("Unable to connect to weather data")
+//     }
+//     else if(response.body.Message){
+//         console.log(response.body.Message)
+//     }
+//     else {
         
+//             const locationkey = response.body.Key
+//             const url1 = 'http://dataservice.accuweather.com/forecasts/v1/daily/1day/' + locationkey + '?apikey=SDhu1AbAnfwcdEeo9m1y67jA3p1AqXXX'
+//             request({ url: url1, json: true }, (error, response) => {
+//                 console.log("Minimum temperature " + response.body.DailyForecasts[0].Temperature.Minimum.Value)
+//                 console.log("Maximum temperature " + response.body.DailyForecasts[0].Temperature.Maximum.Value)
+//             })
+        
+//     }
+// })
+const temperature=(latitude,longitude,callback)=>{
+    // const url = 'http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=SDhu1AbAnfwcdEeo9m1y67jA3p1AqXXX&q=34.0544%2C-118.2439'
+    //const url ='http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=SDhu1AbAnfwcdEeo9m1y67jA3p1AqXXX&q='+encodeURIComponent(latitude,longitude)
+    const url = 'http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=SDhu1AbAnfwcdEeo9m1y67jA3p1AqXXX&q=' + encodeURIComponent(latitude) + '%2C' + encodeURIComponent(longitude)
+    request({url:url, json:true},(error,response)=>{
+        //console.log(response)
+        if(error){
+            callback('Unable to connect to Accuweather',undefined);
+        }
+        
+        // else if(response===null){
+        //     callback('Please enter valid coordinates',undefined)
+        // }
+        else if(response.body.Message){
+            callback(response.body.Message,undefined)
+        }
+        else{
             const locationkey = response.body.Key
             const url1 = 'http://dataservice.accuweather.com/forecasts/v1/daily/1day/' + locationkey + '?apikey=SDhu1AbAnfwcdEeo9m1y67jA3p1AqXXX'
             request({ url: url1, json: true }, (error, response) => {
-                console.log("Minimum temperature " + response.body.DailyForecasts[0].Temperature.Minimum.Value)
-                console.log("Maximum temperature " + response.body.DailyForecasts[0].Temperature.Maximum.Value)
+                callback(undefined,{
+                    minTemp: response.body.DailyForecasts[0].Temperature.Minimum.Value,
+                    maxTemp: response.body.DailyForecasts[0].Temperature.Maximum.Value
+                })
             })
-        
-    }
-    /*
-    */
+        }
+    })
+}
+
+temperature(13.33222, 74.74611,(error,data)=>{
+    console.log(error)
+    console.log(data)
 })
-const url1 ='https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoibWFuanVuYXRoYXBhdGthciIsImEiOiJja2t0NzU5cDUwa2lrMnFwNjlldmR0Ymc4In0.T3OhyQ0q1bG_IWYsd2mpxg'
-//const url1 = 'https://api.mapbox.com/geocoding/v5/mapbox.places/fbhdskjnlms.json?access_token=pk.eyJ1IjoibWFuanVuYXRoYXBhdGthciIsImEiOiJja2t0NzU5cDUwa2lrMnFwNjlldmR0Ymc4In0.T3OhyQ0q1bG_IWYsd2mpxg'
-request({ url: url1, json: true }, (err,response)=>{
-    // console.log(response)
-    if (err){
-        console.log("unable to connect to the mapbox api")
-    }
-    else if(response.body.features.length==0){
-        console.log("please check the entered place name")
-    }
-    else {
-        const res = response.body
-        console.log("latitude " + res.features[0].center[0])
-        console.log("longitude " + res.features[0].center[1])
-    }
-})
+//const url = 'http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=SDhu1AbAnfwcdEeo9m1y67jA3p1AqXXX&q=34.0544%2C-118.2439'
+// latitude = 40.8
+// longitude = -77.8
+// const url = 'http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=SDhu1AbAnfwcdEeo9m1y67jA3p1AqXXX&q=' + encodeURIComponent(latitude)+'%2C'+encodeURIComponent(longitude)
+// console.log(url)
+
+// geocode('nitte',(err,data)=>{
+//     console.log(err)
+//     console.log(data)
+
+// })
---
+++ utils/geocode.js
@@ -0,0 +1,20 @@
+const request=require('request')
+const geocode = (address, callback) => {
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1IjoibWFuanVuYXRoYXBhdGthciIsImEiOiJja2t0NzU5cDUwa2lrMnFwNjlldmR0Ymc4In0.T3OhyQ0q1bG_IWYsd2mpxg'
+    request({ url: url, json: true }, (err, response) => {
+        if (err) {
+            callback('unable to connect to Accuweather API', undefined)
+        }
+        else if (response.body.features.length === 0) {
+            callback('Please enter valid place name', undefined)
+        }
+        else {
+            callback(undefined, {
+                latitude: response.body.features[0].center[0],
+                longitude: response.body.features[0].center[1],
+                place: response.body.features[0].place_name
+            })
+        }
+    })
+}
+module.exports=geocode
