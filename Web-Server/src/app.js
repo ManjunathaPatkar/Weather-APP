d1a9d4551243d5e3643ee6fb48bca5f089d180e9
Manjunatha Patkar <manjunathapatkar150@gmail.com>
2021-02-08 15:17:56 +0530

    adding html instead of text in response

--- Web-Server/src/app.js
+++ Web-Server/src/app.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app=express()
 
 app.get('',(req,res)=>{
-    res.send('Hello express!')
+    res.send('<h1>Weather </h1>')
 })
 app.get('/help',(req,res)=>{
     res.send('Help page')
