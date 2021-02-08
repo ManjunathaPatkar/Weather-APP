d78a72dc78ad3f9eebc2314afc4cb6d3f3f59f57
Manjunatha Patkar <manjunathapatkar150@gmail.com>
2021-02-08 10:29:24 +0530

    fixed bugs in utils/goecode

--- utils/geocode.js
+++ utils/geocode.js
@@ -3,7 +3,7 @@ const geocode = (address, callback) => {
     const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1IjoibWFuanVuYXRoYXBhdGthciIsImEiOiJja2t0NzU5cDUwa2lrMnFwNjlldmR0Ymc4In0.T3OhyQ0q1bG_IWYsd2mpxg'
     request({ url: url, json: true }, (err, response) => {
         if (err) {
-            callback('unable to connect to Accuweather API', undefined)
+            callback('unable to connect to Mapbox API', undefined)
         }
         else if (response.body.features.length === 0) {
             callback('Please enter valid place name', undefined)
