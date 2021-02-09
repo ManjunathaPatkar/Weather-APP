116708087b9105925fb370ac9171d093384b558f
Manjunatha Patkar <manjunathapatkar150@gmail.com>
2021-02-09 14:17:30 +0530

    created 404 view page

--- Web-Server/src/app.js
+++ Web-Server/src/app.js
@@ -47,6 +47,14 @@ app.get('/weather',(req,res)=>{
     })
 })
 
+app.get('/help/*',(req,res)=>{
+    res.send("help article not found")
+})
+
+app.get('*',(req,res)=>{
+    res.send("404 not found")
+})
+
 app.listen(3000,()=>{
     console.log("server started.....")
 })
