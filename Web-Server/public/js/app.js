1f0d7bdcd34b27aa32e0d2368be2c386e4e375c2
Manjunatha Patkar <manjunathapatkar150@gmail.com>
2021-02-10 11:37:14 +0530

    Update app.js

---
+++ Web-Server/public/js/app.js
@@ -0,0 +1,11 @@
+fetch('http://localhost:3000/weather?address=hdbnfjsdlm').then((response)=>{
+    response.json().then((data)=>{
+        if(data.error){
+            console.log(data.error)
+        }
+        else {
+            console.log(data) 
+            }
+        
+    })
+})
